e3d023049aca2f7cef93f1f9a0a4ef2df6f8880f
oothan <mgoothan.psnl@gmail.com>
2026-02-05 16:23:10 +0700

    feat: add storage slots

--- src/StorageSlots.sol
+++ src/StorageSlots.sol
@@ -4,17 +4,26 @@ pragma solidity ^0.8.33;
 import "forge-std/console.sol";
 
 contract StorageSlots {
+
+    uint256 constant num = 45; // not change slots
     uint256 a = 100;                // 0x0
-    uint256 b = type(uint256).max;  // 0x1
-    bool c = true;                  // 0x2    
+    uint256 b;  // 0x1
+    bool c = false;                  // 0x2    
 
     constructor() {
         console.log(a);
 
         uint256 x;
+        bytes32 y;
+        bool z;
         assembly ("memory-safe") {
-            x := sload(0x0)
+            x := sload(0x1)
+            y := sload(0x2)
+            z := sload(0x2)
         }
         console.log(x); 
+        console.log(b);
+        console.logBytes32(y);
+        console.log(z);
     }
 }
